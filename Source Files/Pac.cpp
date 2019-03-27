1d9f5d7bf5aec4ae9de10f54c557f6321b56541f
Sam Montanaro <sammontanaro12@gmail.com>
2019-03-27 01:29:05 -0400

    Fixed drawEntity function, added drawClearEntity function

--- Source Files/Pac.cpp
+++ Source Files/Pac.cpp
@@ -7,19 +7,24 @@ using namespace std;
 
 HDC hdc = GetDC(GetConsoleWindow());
 
-void drawEntity(int sprite[16][16]), input(), logic();
+enum keyboardInput { UP, DOWN, LEFT, RIGHT, START, NONE };
+keyboardInput playerInput = NONE, lastPlayerInput = NONE;
+
+void drawEntity(int sprite[16][16]);
+void drawClearEntity(keyboardInput playerInput);
+void input();
+void logic();
 bool keyIsDown(char key, bool pressed = true, bool held = true);
 double getTime(); //Returns timer with nanosecond precision
 double getTimeSince(double); //Returns timer with nanosecond precision since a point in time
 double wait(double); //Pauses for the given amount of seconds, returns how much extra time was waited
 void rotate(int sprite[16][16], int);
 
-enum keyboardInput { UP, DOWN, LEFT, RIGHT, START, NONE };
-keyboardInput playerInput = NONE, lastPlayerInput = NONE;
 int f_count = 0;
 
 COLORREF yellow = RGB(255, 255, 0);
 COLORREF blue = RGB(0, 0, 255);
+COLORREF black = RGB(12, 12, 12);
 
 int playerX = 40, playerY = 40;
 
@@ -100,6 +105,7 @@ int wall[16][16] = {
 
 int main() {
 	while (timer += (dt = FPS + wait(FPS))) {
+		drawClearEntity(playerInput);
 		if (f_count == 1)
 			drawEntity(PacMan_F1);
 		if (f_count == 2)
@@ -108,7 +114,6 @@ int main() {
 			drawEntity(PacMan_F3);
 		input();
 		logic();
-		system("CLS");
 	}
 	return 0;
 }
@@ -155,8 +160,6 @@ void logic() {
 		playerY += 5;
 		break;
 	}
-
-
 }
 
 void rotate(int sprite[16][16], int deg) {
@@ -193,23 +196,37 @@ void rotate(int sprite[16][16], int deg) {
 }
 
 void drawEntity(int sprite[16][16]) {
-	for (int y = 0; y < 16; y++) {
-		for (int x = 0; x < 16; x++) {
-			switch (lastPlayerInput) {
-			case RIGHT:
-			case NONE:
-				rotate(sprite, 0);
-				break;
-			case DOWN:
-				rotate(sprite, 90);
-				break;
-			case LEFT:
-				rotate(sprite, 180);
-				break;
-			case UP:
-				rotate(sprite, 270);
-				break;
-			}
+	switch (lastPlayerInput) {
+	case RIGHT:
+	case NONE:
+		rotate(sprite, 0);
+		break;
+	case DOWN:
+		rotate(sprite, 90);
+		break;
+	case LEFT:
+		rotate(sprite, 180);
+		break;
+	case UP:
+		rotate(sprite, 270);
+		break;
+	}
+}
+
+void drawClearEntity(keyboardInput playerInput) {
+	int xOffset = 0, yOffset = 0;
+	if (playerInput == LEFT)
+		xOffset = 5;
+	else if (playerInput == RIGHT)
+		xOffset = -5;
+	else if (playerInput == UP)
+		yOffset = 5;
+	else if (playerInput == DOWN)
+		yOffset = -5;
+
+	for (int i = 0; i < 16; i++) {
+		for (int j = 0; j < 16; j++) {
+			SetPixel(hdc, playerX + xOffset + j, playerY + yOffset + i, black);
 		}
 	}
 }
