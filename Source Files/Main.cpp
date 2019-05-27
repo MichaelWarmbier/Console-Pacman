b42fb9caa3d5c4e9984b4af6398e857de1aa2382
Sam Montanaro <sammontanaro12@gmail.com>
2019-05-27 02:01:37 -0400

    Update Main with FPS Counter INSIDE Game Loop

--- Source Files/Main.cpp
+++ Source Files/Main.cpp
@@ -1,10 +1,15 @@
 #include "Implementation.h"
+
 int main() {
+	ShowConsoleCursor(false);
 	bool EXIT_GAME = false;
+	TIMER_INIT
 	while (!EXIT_GAME && (timer += (dt = FPS + wait(FPS)))) {
+		TIMER_START
 		GameDraw();
 		GameInput();
 		GameLogic();
+		TIMER_STOP	
 	}
 	return EXIT_SUCCESS;
 }
