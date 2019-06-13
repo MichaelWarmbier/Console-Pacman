b4aa727f009250d67cba77af1cc711905b330f19
Sam Montanaro <sammontanaro12@gmail.com>
2019-06-12 21:43:07 -0400

    Update Main.cpp

--- Source Files/Main.cpp
+++ Source Files/Main.cpp
@@ -1,15 +1,17 @@
 #include "Implementation.h"
 
 int main() {
+	system("MODE 29, 16");
 	ShowConsoleCursor(false);
 	bool EXIT_GAME = false;
+	//SetGraphicsMode(Pac.hdc, GM_ADVANCED);
 	TIMER_INIT
 	while (!EXIT_GAME && (timer += (dt = FPS + wait(FPS)))) {
 		TIMER_START
 		GameDraw();
 		GameInput();
 		GameLogic();
-		TIMER_STOP	
+		TIMER_STOP
 	}
 	return EXIT_SUCCESS;
 }
