5c1453bfc92a920adc71a1fa94067e5d9f601250
Michael Warmbier <MichaelCWarmbier@gmail.com>
2019-06-18 16:33:30 -0400

    Added pause functionality

--- Source Files/Main.cpp
+++ Source Files/Main.cpp
@@ -1,17 +1,18 @@
 #include "Implementation.h"
 
 int main() {
-	system("MODE 29, 16");
+	system("MODE 34, 20");
 	ShowConsoleCursor(false);
 	bool EXIT_GAME = false;
 	//SetGraphicsMode(Pac.hdc, GM_ADVANCED);
 	TIMER_INIT
-	while (!EXIT_GAME && (timer += (dt = FPS + wait(FPS)))) {
-		TIMER_START
-		GameDraw();
-		GameInput();
-		GameLogic();
-		TIMER_STOP
-	}
+		while (!EXIT_GAME && (timer += (dt = FPS + wait(FPS)))) {
+			TIMER_START
+				GameDraw();
+			GameInput();
+			if (GameState != LIMBO)
+				GameLogic();
+			TIMER_STOP
+		}
 	return EXIT_SUCCESS;
 }
