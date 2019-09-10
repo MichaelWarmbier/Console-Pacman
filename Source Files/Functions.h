1209b4da3465e9d9b36918d20c12598da19c35ec
Michael Warmbier <MichaelCWarmbier@gmail.com>
2019-09-09 20:09:33 -0400

    Create Functions.h

---
+++ Source Files/Functions.h
@@ -0,0 +1,67 @@
+#pragma once
+#include "Data.h"
+
+bool KeyIsDown(char key, bool pressed, bool held) {
+	int keyState = GetAsyncKeyState(static_cast<int>(key));
+	return (pressed && (keyState & 1)) || (held && (keyState & 0xA000));
+}
+
+double GetTime() {
+	return time_point_cast<nanoseconds>(high_resolution_clock::now()).time_since_epoch().count() / 1e9;
+}
+
+double GetTimeSince(double startTime) {
+	return time_point_cast<nanoseconds>(high_resolution_clock::now()).time_since_epoch().count() / 1e9 - startTime;
+}
+
+double Wait(double waitTime) {
+	double startTime = GetTime();
+
+	while (waitTime > GetTimeSince(startTime)) {}
+
+	return GetTimeSince(startTime + waitTime);
+}
+
+void ShowConsoleCursor(bool showFlag)
+{
+	HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	CONSOLE_CURSOR_INFO     cursorInfo;
+
+	GetConsoleCursorInfo(out, &cursorInfo);
+	cursorInfo.bVisible = showFlag;
+	SetConsoleCursorInfo(out, &cursorInfo);
+}
+
+void SetConsoleSize(int x, int y) {
+	HWND console = GetConsoleWindow();
+	HMONITOR monitor = MonitorFromWindow(console, MONITOR_DEFAULTTOPRIMARY);
+
+	MONITORINFOEX miex;
+	miex.cbSize = sizeof(miex);
+	GetMonitorInfo(monitor, &miex);
+	int cxLogical = (miex.rcMonitor.right - miex.rcMonitor.left);
+	int cyLogical = (miex.rcMonitor.bottom - miex.rcMonitor.top);
+
+	DEVMODE dm;
+	dm.dmSize = sizeof(dm);
+	dm.dmDriverExtra = 0;
+	EnumDisplaySettings(miex.szDevice, ENUM_CURRENT_SETTINGS, &dm);
+	int cxPhysical = dm.dmPelsWidth;
+	int cyPhysical = dm.dmPelsHeight;
+
+	double horzScale = ((double)cxPhysical / (double)cxLogical);
+	double vertScale = ((double)cyPhysical / (double)cyLogical);
+
+	SetWindowPos(console, HWND_TOP, 0, 0, double(x * 16.0) / horzScale + 10, double(y * 16.0) / vertScale, SWP_NOMOVE); // Resize without moving where the console window was placed
+
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO info;
+	GetConsoleScreenBufferInfo(handle, &info);
+	COORD new_size = {
+		info.srWindow.Right - info.srWindow.Left + 1,
+		info.srWindow.Bottom - info.srWindow.Top + 1
+	};
+	SetConsoleScreenBufferSize(handle, new_size);
+	ShowConsoleCursor(false);
+}
