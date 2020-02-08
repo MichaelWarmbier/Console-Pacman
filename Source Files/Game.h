1cf2ecd6d33da45bf509d48fb908f2eec41bdf0c
Michael Warmbier <MichaelCWarmbier@gmail.com>
2020-02-07 21:17:50 -0500

    Update Game.h

--- Source Files/Game.h
+++ Source Files/Game.h
@@ -43,38 +43,58 @@ private:
 	291,297,297,297,297,297,297,297,297,297,297,297,297,297,297,297,297,297,297,297,297,297,297,297,297,297,297,290,
 	000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,
 	000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,
-	};
-	bool EnergizerFlashActive, _1UPFlashActive;
-	double EnergizerFlashTS, _1UPFlashTS;
+	}; 
+	// Primary Board
+	bool EnergizerFlashActive, _1UPFlashActive; // Stores activity of Energizer and 1UP flash respectively
+	double EnergizerFlashTS, _1UPFlashTS; // Time stamps for Energizer and 1UP flash respectively
+	bool GameStarted; // Stores if game has passed READY screen
 
-	int Score;
-	int HighScore;
-	int Lives;
-	int Level;
+	int DotsCollected; // Collected dots value
 
-	const int FruitTable[20] = {407,407,411,415,415,419,419,423,423,427,427,431,431,435,435,435,435,435,435,435};
+	// DEBUG
+	bool DisplayFPS = false; // Displays FPS meter
+	bool InstaDeath = false; // Instant beath button bound to '3'
+	bool InstaLive = false; // Instant life button bound to '2'
+	bool InstaEnd = false; // Instant win button bound to '1'
+
+	const int FruitTable[20] = {407,407,411,415,415,419,419,423,423,427,427,431,431,435,435,435,435,435,435,435}; // Table for determining which fruit to draw per level
 
 public:
-	Game() :EndGame(false),EnergizerFlashActive(false),EnergizerFlashTS(GetTime()),_1UPFlashActive(false),_1UPFlashTS(GetTime()),Score(0),HighScore(0),Lives(3),Level(1) {
-		InitializeCollision();
-		DrawLives(Lives); // Lives
-		DrawLevel(Level); // Level
+	int Score; // Score value
+	int HighScore; // Highscore value
+	int Lives; // Lives value
+	int Level; // Level value
+	bool Death; // Bool to check if player died or won a level
+	double FPSTS; int FPS, LastFPS; // FPS related variables
+
+	Game(int Lvl, int Lv, int Scre) :EndGame(false), Death(false), GameStarted(false), EnergizerFlashActive(false), EnergizerFlashTS(GetTime()), _1UPFlashActive(false), _1UPFlashTS(GetTime()), Score(Scre), HighScore(0), Lives(Lv), Level(Lvl), DotsCollected(0), FPSTS(GetTime()), FPS(0), LastFPS(0) {
+		InitializeCollision(); // Provides map collision
+		DrawLevel(); // Level
 	};
-	bool GameStatus() const;
-	void SetTile(int xpos, int ypos, int ID);
-	bool UpdateTS(bool & State, double & TS, double Time);
-	void DrawCounter(int xpos, int ypos, int value);
-	int NumberToSprite(int value) const;
-	void DrawLives(int L);
-	void DrawLevel(int L);
-	void MovePacman();
-	void InitializeCollision();
 
-	void Draw();
-	void Input();
-	void Logic();
+	bool GameStatus() const; // Returns the status of the game
+	void SetTile(int xpos, int ypos, int ID); // Sets a specified tile to a specified value
+	bool UpdateTS(bool & State, double & TS, double Time); // Updates a given time stamp every amount of given time and updates its state accordingly
+	void DrawCounter(int xpos, int ypos, int value); // Draws an integer counter for a given value
+	int NumberToSprite(int value) const; // Converts integer to its respective sprite value
+	void DrawLives(); // Draws life display
+	void DrawLevel(); // Draws level display
+	void MovePacman(); // Increments Pacman in face direction
+	void InitializeCollision(); // Creates collision board based off current board
+	void TeleportPlayer(); // Teleports player when touching specific positions
+	void EatObjects(); // Applies logic regarding eating power pellets and energizers
+	void LevelEnd(); // Ends the current level if the total dot count is 244
+	void ClearCollisionTile(int xpos, int ypos); // Clears a tiles data on the collision board
+	int GetFPS(); // Returns the current FPS
+	void Flash(); // Flashes the map to its alternative color scheme
+	void KillPlayer(); // Kills the player
+	void GetReady(); // Displays ready text
 
-	void DrawBoard();
+	void Draw(); // Draws all display elements
+	void Input(); // Rerceives input used for future logic
+	void Logic(); // Applies all game logic
+
+	void DrawBoard(); // Draws specifically the board
 
 };
 
