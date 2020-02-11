3abf050019baf1818ce4b919dca92a49bed38908
Michael Warmbier <MichaelCWarmbier@gmail.com>
2020-02-11 00:18:51 -0500

    Update TitleScreen.h

--- Source Files/TitleScreen.h
+++ Source Files/TitleScreen.h
@@ -1,13 +1,26 @@
 #pragma once
-#include "Header.h"
+#include "Main.h"
 class TitleScreen {
 private:
-	int OptSel;
-	int Screen;
+	bool EndMenu;
+	int CursorSel; // Stores the currently selected option
+	double GhostTS, PacmanTS;
+	int PacmanPhase;
+	bool GhostFrame;
+
 public:
+	bool EXIT;
+
+	bool MenuStatus() const { return EndMenu; }; // Rerturn program status
+	void ExitMenu() { EndMenu = true; }; // Exit program
+	TitleScreen() : CursorSel(1), EXIT(false), EndMenu(false), GhostTS(GetTime()), GhostFrame(0), PacmanPhase(0), PacmanTS(GetTime()) { }
+
 	void Draw();
 	void Input();
 	void Logic();
-	bool MoveCursor(dir VDirection);
+
+	void StringToSprite(const char text[50], int xpos, int ypos, COLOR color) const;
+	int CharToSprite(char c, COLOR color) const;
+	int CursorLogic();
 };
 
